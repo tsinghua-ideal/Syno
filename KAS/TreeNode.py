bbcd2e77392979d7c0d1fc24b83d96506a210dea
timsu1104 <timsu20021104@gmail.com>
2023-05-10 10:38:41 +0800

    Tree Node.

---
+++ KAS/TreeNode.py
@@ -0,0 +1,111 @@
+from collections import defaultdict
+from typing import List, Union
+
+from .Node import Path, Node, PseudoNext, AbsolutePath
+from .Sampler import Sampler
+
+import kas_cpp_bindings
+from kas_cpp_bindings import Next
+
+PseudoTreeNext = Union[PseudoNext, Next.Type]
+
+
+class TreePath(Path):
+    """
+    A path that decouples the type and content of a node.
+    Type: Next.Type
+    Content: Hash value (currently) or Parameters 
+    """
+
+    def to_next(self, tup: PseudoTreeNext) -> Next:
+        if isinstance(tup, Next.Type):
+            return Next(tup, 0)
+        return super().to_next(tup)
+
+    def __init__(self, path: List[PseudoTreeNext]) -> None:
+        """abs_path records [(op, hash)]"""
+        self.abs_path: AbsolutePath = [self.to_next(n) for n in path]
+
+    def __getitem__(self, key):
+        return self.abs_path[key]
+
+    def append(self, next: PseudoTreeNext):
+        self.abs_path.append(self.to_next(next))
+
+    def concat(self, next: PseudoTreeNext) -> 'TreePath':
+        if isinstance(next, Next.Type):
+            return TreePath(self.abs_path + [self.to_next(next)])
+        else:
+            return TreePath(self.abs_path[:-1] + [Next(self.abs_path[-1].type, next)])
+
+    def path_to_strs(self, sampler: Sampler):
+        full_path = self.abs_path
+        suffix = ''
+        if len(self.abs_path) > 0 and self.abs_path[-1].key == 0:
+            suffix = str(self.abs_path[-1].type)
+            full_path = self.abs_path[:-1]
+        return sampler.path_to_strs(full_path), suffix if suffix != '' else sampler.path_to_strs(full_path)
+
+
+class TreeNode(Node):
+    """
+    A node that represents either a type or a full node
+    """
+
+    def __init__(self, path: Path, node: kas_cpp_bindings.Node, is_mid: bool = False, type: Next.Type = None) -> None:
+        """
+        node: the underlying node of this node or of its father (if it is a mid node). 
+        """
+        super().__init__(node)
+        self.path = TreePath(path)
+        self._is_mid = is_mid
+        self._type = type
+
+        handles = super().get_children_handles()
+        primitives = defaultdict(list)
+        for handle in handles:
+            primitives[handle.type].append(handle.key)
+
+        if is_mid:  # calculate the children upon initialization
+            assert type is not None, "Please pass a type for mid nodes."
+            self.children = primitives[type]
+        else:
+            self.children = list(primitives.keys())
+
+    def __eq__(self, __value: object) -> bool:
+        return \
+            super().__eq__(__value) and \
+            self._is_mid == __value._is_mid and \
+            self._type == __value._type
+
+    def __hash__(self) -> int:
+        return hash(self._node) + hash(self._is_mid) + hash(self._type)
+
+    def children_count(self) -> int:
+        """Get the number of all children of a node."""
+        return len(self.children)
+
+    def get_children_handles(self) -> List[int]:
+        """Get all children of a node."""
+        return self.children
+
+    def get_child(self, next: PseudoTreeNext) -> 'TreeNode':
+        """Get the child node of a node with a Next."""
+        new_path = self.path.concat(next)
+        if self._is_mid:
+            assert not isinstance(next, Next.Type)
+            return TreeNode(new_path, self._node.get_child(new_path[-1]))
+        else:
+            assert isinstance(next, Next.Type)
+            return TreeNode(new_path, self._node, is_mid=True, type=next)
+
+    def is_final(self) -> bool:
+        """Check if a node is final, which means it can be realized as a Halide kernel."""
+        if self._is_mid:
+            return False
+        return self._node.is_final()
+
+    def __repr__(self) -> str:
+        if self._is_mid:
+            return str(self._node) + '->' + str(self._type)
+        return str(self._node)
--- tests/test_mcts.py
+++ tests/test_mcts.py
@@ -14,25 +14,28 @@ class Model(nn.Module):
         x = self.kernel(x)
         return x
 
+
 def test_mcts():
     net = Model()
-    sampler = Sampler("[H,W]", "[H,W]", ["H = 128: 1", "W: 3"], ["s_1=2: 2", "k_1=3", "4"], net=net, depth=5, cuda=False, autoscheduler=CodeGenOptions.Li2018)
+    sampler = Sampler("[H,W]", "[H,W]", ["H = 128: 1", "W: 3"], [
+                      "s_1=2: 2", "k_1=3", "4"], net=net, depth=5, cuda=False, autoscheduler=CodeGenOptions.Li2018)
     mcts = MCTS(sampler)
     in_tensor = torch.randn((128, 128))
     for idx in range(10):
         receipt, node = mcts.do_rollout(sampler.root())
         _, path = receipt
         print(f"Iteration {idx}. Sampled {node.path} for {path}:")
-        print(sampler.path_to_strs(path))
+        print(path.path_to_strs(sampler))
         for i in range(len(path)):
             child = sampler.visit(Path(path.abs_path[:i]))
             print(f"Node {child} has children:", child.get_children_types())
-        kernel_packs, _ = sampler.realize(net, node, f"test_mcts_{idx}")
-        sampler.replace(net, kernel_packs)
-        print(f"Computing forward {idx}...")
-        print(f"Result: {net(in_tensor)}")
+        # kernel_packs, _ = sampler.realize(net, node, f"test_mcts_{idx}")
+        # sampler.replace(net, kernel_packs)
+        # print(f"Computing forward {idx}...")
+        # print(f"Result: {net(in_tensor)}")
         mcts.back_propagate(receipt, 1.0)
 
+
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
     test_mcts()
