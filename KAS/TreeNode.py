05180c3f6799e4417fd49f290508ee9b73f0bd6a
timsu1104 <timsu20021104@gmail.com>
2023-07-01 17:14:37 +0800

    FUSE: add edge.

--- KAS/TreeNode.py
+++ KAS/TreeNode.py
@@ -3,6 +3,7 @@ import logging
 import math
 from statistics import mean, stdev
 from collections import defaultdict
+from functools import partial
 
 from .Node import Path, Node, PseudoNext, AbsolutePath
 from .Sampler import Sampler
@@ -25,34 +26,34 @@ class TreePath(Path):
         return super().to_next(tup)
 
     @ staticmethod
-    def deserialize(serialized: str) -> 'TreePath':
-        deserialized_list = serialized.split('_')
+    def deserialize(serialized: str) -> "TreePath":
+        deserialized_list = serialized.split("_")
         return TreePath([Next(Next.Type(int(n[0])), int(n[1:])) for n in deserialized_list])
 
     @staticmethod
     def decode_next_type(repr_: str):
-        if repr_ == 'MapReduce':
-            return '0'
+        if repr_ == "MapReduce":
+            return "0"
         elif repr_ == "Shift":
-            return '1'
+            return "1"
         elif repr_ == "Stride":
-            return '2'
+            return "2"
         elif repr_ == "Split":
-            return '3'
+            return "3"
         elif repr_ == "Unfold":
-            return '4'
+            return "4"
         elif repr_ == "Merge":
-            return '5'
+            return "5"
         elif repr_ == "Share":
-            return '6'
+            return "6"
         elif repr_ == "Finalize":
-            return '7'
+            return "7"
 
     @ staticmethod
-    def decode_str(str_repr: str) -> 'TreePath':
-        str_repr = str_repr[1:-1].split(', ')
-        str_repr = '_'.join([TreePath.decode_next_type(
-            r[:-1].split('(')[0])+r[:-1].split('(')[1] for r in str_repr])
+    def decode_str(str_repr: str) -> "TreePath":
+        str_repr = str_repr[1:-1].split(", ")
+        str_repr = "_".join([TreePath.decode_next_type(
+            r[:-1].split("(")[0])+r[:-1].split("(")[1] for r in str_repr])
         return TreePath.deserialize(str_repr)
 
     def __init__(self, path: List[PseudoTreeNext]) -> None:
@@ -62,7 +63,7 @@ class TreePath(Path):
     def __getitem__(self, key):
         return self.abs_path[key]
 
-    def concat(self, next: PseudoTreeNext) -> 'TreePath':
+    def concat(self, next: PseudoTreeNext) -> "TreePath":
         if isinstance(next, Next.Type):
             return TreePath(self.abs_path + [Next(next, 0)])
         elif isinstance(next, int):
@@ -75,11 +76,11 @@ class TreePath(Path):
 
     def path_to_strs(self, sampler: Sampler):
         full_path = self.abs_path
-        suffix = ''
+        suffix = ""
         if len(self.abs_path) > 0 and self.abs_path[-1].key == 0:
             suffix = str(self.abs_path[-1].type)
             full_path = self.abs_path[:-1]
-        return sampler.path_to_strs(full_path), suffix if suffix != '' else sampler.path_to_strs(full_path)
+        return sampler.path_to_strs(full_path), suffix if suffix != "" else sampler.path_to_strs(full_path)
 
 
 class TreeNode:
@@ -97,26 +98,22 @@ class TreeNode:
             - node
             - is_mid = False
             - children: Dict[Next, TreeNode]
-            - n
-            - q
+            - state: AverageMeter
             - NumVisitToChild
         2. mid node: a node that represents a type.
             - node
             - is_mid = True
             - type
-            - n
-            - q
+            - state: AverageMeter
             - NumVisitToChild
         """
         # identifications
-        self._node = node
+        self._node: Node = node
         self._is_mid: bool = is_mid
         self._type: Next.Type = type
         
         # states
-        self.sum: float = 0
-        self.sumsq: float = 0
-        self.N: float = 0
+        self.state = AverageMeter(support_std=True)
         
         self._last_T: int = 0
         self.l_rave: Dict[PseudoArc, AverageMeter] = defaultdict(AverageMeter)
@@ -129,17 +126,15 @@ class TreeNode:
         if node.is_final():
             self.reward: float = -1
             self.filtered: bool = False
-        if not self._is_mid:
-            self.children: List['TreeNode'] = []
-
-        assert isinstance(self._node, Node)
-        primitives = self._node.collect_operations()
-        # Initialize TreeNodes for children.
-        if not self._is_mid:
+        if self.is_mid:
+            self.edge_states: Dict[int, AverageMeter] = defaultdict(partial(AverageMeter, support_std=True))
+        else:
+            self.children: List["TreeNode"] = []
+            primitives = self._node.collect_operations()
             for child in primitives.keys():
                 self.children.append(TreeNode(node, is_mid=True, type=child))
 
-    def __eq__(self, __value: 'TreeNode') -> bool:
+    def __eq__(self, __value: "TreeNode") -> bool:
         eq_flag = self._node.__eq__(__value._node) and \
             self._is_mid == __value._is_mid and \
             self._type == __value._type and \
@@ -151,43 +146,43 @@ class TreeNode:
     def __hash__(self) -> int:
         return hash((self.to_node(), self._is_mid, self._type))
     
-        
     @property
     def mean(self) -> float:
-        return self.sum / self.N if self.N > 0 else 0
+        return self.state.mean
     
     @property
     def std(self) -> float:
-        return self.sumsq / self.N - self.mean * self.mean if self.N > 1 else 0
+        return self.state.std
     
     def update(self, reward: float, arc: PseudoArc=None) -> None:
-        self.N += 1
-        self.sum += reward
-        self.sumsq += reward * reward
+        self.state.update(reward)
         if arc:
+            assert not self.is_final()
             self.l_rave[arc].update(reward)
+            if self._is_mid:
+                assert isinstance(arc, Arc)
+                nxt = arc.to_next()
+                self.edge_states[nxt.key].update(reward)
             
     @property
     def state_dict(self) -> Dict:
         return {
-            'N': self.N,
-            'sum': self.sum,
-            'sumsq': self.sumsq,
-            '_last_T': self._last_T,
-            '_is_dead': self._is_dead,
-            '_isin_tree': self._isin_tree
+            "state": self.state.serialize(),
+            "_last_T": self._last_T,
+            "_is_dead": self._is_dead,
+            "_isin_tree": self._isin_tree
         }
         
     def load(self, state_dict: Dict) -> Dict:
-        self.N = state_dict['N']
-        self.sum = state_dict['sum']
-        self.sumsq = state_dict['sumsq']
-        self._last_T = state_dict['_last_T']
-        self._is_dead = state_dict['_is_dead']
-        self._isin_tree = state_dict['_isin_tree']
+        self.state = AverageMeter.deserialize(state_dict["state"])
+        self._last_T = state_dict["_last_T"]
+        self._is_dead = state_dict["_is_dead"]
+        self._isin_tree = state_dict["_isin_tree"]
 
-    def children_count(self, factory: Dict[Node, 'TreeNode'], on_tree: bool=False) -> int:
-        """Get the number of all children of a node."""
+    def children_count(self, factory: Dict[Node, "TreeNode"], on_tree: bool=False) -> int:
+        """
+        Get the number of all children of a node.
+        """
         primitives = self._node.collect_operations()
         if self._is_mid:
             nexts = primitives[self._type]
@@ -204,7 +199,7 @@ class TreeNode:
         else:
             return len(self.children) - sum([child.is_dead_end(factory) or (on_tree and not child._isin_tree) for child in self.children])
 
-    def is_fully_in_tree(self, factory: Dict[Node, 'TreeNode']) -> bool:
+    def is_fully_in_tree(self, factory: Dict[Node, "TreeNode"]) -> bool:
         """
         Get all nexts of a node. 
         """
@@ -219,7 +214,7 @@ class TreeNode:
         else:
             return all([c._isin_tree for _, c in self.get_children(factory)])
     
-    def flush_T(self, T:int, factory: Dict[Node, 'TreeNode'], g_rave: Dict[Arc, AverageMeter], c_l: float, b: float) -> None:
+    def flush_T(self, T:int, factory: Dict[Node, "TreeNode"], g_rave: Dict[Arc, AverageMeter], c_l: float, b: float) -> None:
         Tp = math.floor(T ** b)
         orig_Tp = math.floor(self._last_T ** b)
         if Tp > orig_Tp:
@@ -228,10 +223,9 @@ class TreeNode:
                     self.add_new_children(factory, g_rave, c_l)
         self._last_T = T
     
-    def add_new_children(self, factory: Dict[Node, 'TreeNode'], g_rave: Dict[Arc, AverageMeter], c_l: float) -> None:
+    def add_new_children(self, factory: Dict[Node, "TreeNode"], g_rave: Dict[Arc, AverageMeter], c_l: float) -> None:
         """
         Add a new children.
-        TOTST
         """
         logging.debug("Add new children to {}".format(self))
         assert not self.is_fully_in_tree(factory)
@@ -246,7 +240,7 @@ class TreeNode:
             else:
                 arc = next
             beta = c_l / (c_l + self.l_rave[arc].N)
-            return (1 - beta) * self.l_rave[arc].avg + beta * g_rave[arc].avg
+            return (1 - beta) * self.l_rave[arc].mean + beta * g_rave[arc].mean
             
         unadded_children = self.get_unadded_children(factory)
         if len(unadded_children) == 0:
@@ -255,18 +249,18 @@ class TreeNode:
         _, child = max(unadded_children, key=rave)
         child._isin_tree = True
 
-    def get_unadded_children(self, factory: Dict[Node, 'TreeNode']) -> List[Tuple[PseudoTreeNext, 'TreeNode']]:
+    def get_unadded_children(self, factory: Dict[Node, "TreeNode"]) -> List[Tuple[PseudoTreeNext, "TreeNode"]]:
         children = self.get_children(factory)
         unadded_children = [child for child in children if not child[1]._isin_tree]
         return unadded_children
     
-    def get_unexpanded_children(self, factory: Dict[Node, 'TreeNode'], on_tree: bool=False) -> List[Tuple[PseudoTreeNext, 'TreeNode']]:
+    def get_unexpanded_children(self, factory: Dict[Node, "TreeNode"], on_tree: bool=False) -> List[Tuple[PseudoTreeNext, "TreeNode"]]:
         children = self.get_children(factory)
         unexpanded_children = [child for child in children if child[1].N == 0]
         if on_tree: return [child for child in unexpanded_children if child[1]._isin_tree]
         return unexpanded_children
     
-    def get_children(self, factory: Dict[Node, 'TreeNode'], auto_initialize: bool=True, on_tree: bool=False) -> List[Tuple[PseudoTreeNext, 'TreeNode']]:
+    def get_children(self, factory: Dict[Node, "TreeNode"], auto_initialize: bool=True, on_tree: bool=False) -> List[Tuple[PseudoTreeNext, "TreeNode", AverageMeter]]:
         """
         Get all children of a node plus the nexts. Since the tree is searching in the background, we shall get the handles frequently. 
         If some children is dead, we remove them
@@ -279,12 +273,13 @@ class TreeNode:
             
             # Remove filtered and dead children. 
             filtered = [
-                (nxt, c) 
-                for c, nxt in zip(children, nexts) 
+                (nxt, c, edge) 
+                for (c, edge), nxt in zip(children, nexts) 
                     if (c is not None) and not c.is_dead_end(factory)
             ]
-            nexts = [nxt for nxt, _ in filtered]
-            children = [c for _, c in filtered]
+            nexts = [nxt for nxt, _, _ in filtered]
+            children = [c for _, c, _ in filtered]
+            edge_states = [edge for _, _, edge in filtered]
         else:
             children = self.children
             children = [
@@ -293,15 +288,16 @@ class TreeNode:
                 if child._type in primitives.keys() and not child.is_dead_end(factory)
             ]
             nexts = [child._type for child in children]
+            edge_states = [child.state for child in children]
             self.children = children
-        assert len(nexts) == len(children)
+        assert len(nexts) == len(children) == len(edge_states)
         if auto_initialize and not on_tree and len(children) == 0: # No children exists
             self._is_dead = True
         if on_tree:
             return [(nxt, c) for nxt, c in zip(nexts, children) if c._isin_tree]
-        return list(zip(nexts, children))
+        return list(zip(nexts, children, edge_states))
 
-    def get_child(self, next: PseudoTreeNext, factory: Dict[Node, 'TreeNode'] = None, auto_initialize: bool=True, on_tree: bool=False) -> Optional['TreeNode']:
+    def get_child(self, next: PseudoTreeNext, factory: Dict[Node, "TreeNode"] = None, auto_initialize: bool=True, on_tree: bool=False) -> Optional[Tuple["TreeNode", AverageMeter]]:
         """
         Get the child node of a node with a Next. When the node is dead, return None.
         """
@@ -317,19 +313,19 @@ class TreeNode:
                     factory[child] = TreeNode(child.to_node())
                 else:
                     return None
-            return factory[child]
+            return factory[child], self.edge_states[next]
         else:
             assert isinstance(next, Next.Type)
-            for _, child in self.get_children(factory):
+            for _, child, edge_state in self.get_children(factory):
                 if child._type == next:
-                    return child if child._isin_tree else None
+                    return child, edge_state if child._isin_tree else None
             return None
 
-    def is_terminal(self, factory: Dict[Node, 'TreeNode']) -> bool:
+    def is_terminal(self, factory: Dict[Node, "TreeNode"]) -> bool:
         """Check if a node is final, which means it can be realized as a Halide kernel."""
         return self.is_final() or self.is_dead_end(factory)
     
-    def is_dead_end(self, factory: Dict[Node, 'TreeNode']) -> bool:
+    def is_dead_end(self, factory: Dict[Node, "TreeNode"]) -> bool:
         """Check if a node is final, which means it can be realized as a Halide kernel."""
         if self.is_final() and not self.filtered:
             return False
@@ -365,10 +361,10 @@ class TreeNode:
             return False
         return self._node.is_final()
     
-    def to_node(self) -> 'Node':
+    def to_node(self) -> "Node":
         return self._node.to_node()
 
     def __repr__(self) -> str:
         if self._is_mid:
-            return str(self._node) + '->' + str(self._type).split('.')[-1]
+            return str(self._node) + "->" + str(self._type).split(".")[-1]
         return str(self._node)
