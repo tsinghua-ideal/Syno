1677edb4bb6afaf9c2b327ba31dcbdfa0880ddd4
Chenggang Zhao <lyricz@yeah.net>
2023-07-10 01:09:08 +0800

    Change experiment settings

--- experiments/launch_mcts_client.sh
+++ experiments/launch_mcts_client.sh
@@ -1,2 +1,2 @@
 # TODO: launch multiple clients with TMUX
-python mcts_client.py --model FCNet --dataset torch/mnist --mean 0.1307 --std 0.3081 --sched cosine --epoch 25 --batch-size 512 --warmup-epochs 0 --cooldown-epochs 5 --lr 0.1 --warmup-lr 0.01 --momentum 0.9 --weight-decay 0.001 --fetch-all-to-gpu "$@"
+python mcts_client.py --model FCNet --dataset torch/mnist --mean 0.1307 --std 0.3081 --sched cosine --epoch 40 --batch-size 512 --warmup-epochs 0 --cooldown-epochs 10 --lr 0.3 --momentum 0.9 --weight-decay 0.001 --fetch-all-to-gpu "$@"
