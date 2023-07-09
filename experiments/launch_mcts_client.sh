e6c4ebb984f15e4a1d27d9616627032d31650e65
Chenggang Zhao <lyricz@yeah.net>
2023-07-10 00:41:27 +0800

    Fix script

--- experiments/launch_mcts_client.sh
+++ experiments/launch_mcts_client.sh
@@ -1,2 +1,2 @@
 # TODO: launch multiple clients with TMUX
-python mcts_client.py --model FCNet --dataset torch/mnist --mean 0.1307 --std 0.3081 --sched cosine --epoch 5 --batch-size 512 --warmup-epochs 0 --cooldown-epochs 0 --lr 0.1 --warmup-lr 0.01 --momentum 0.9 --weight-decay 0.001 $1
+python mcts_client.py --model FCNet --dataset torch/mnist --mean 0.1307 --std 0.3081 --sched cosine --epoch 25 --batch-size 512 --warmup-epochs 0 --cooldown-epochs 5 --lr 0.1 --warmup-lr 0.01 --momentum 0.9 --weight-decay 0.001 --fetch-all-to-gpu "$@"
