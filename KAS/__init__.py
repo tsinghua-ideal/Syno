83cbf94b3dc7a609bff504760167a27fce79199f
Yongqi-Zhuo <Yongqi-Zhuo@users.noreply.github.com>
2023-07-20 23:20:05 +0800

    Bring Halide runtime back.

--- KAS/__init__.py
+++ KAS/__init__.py
@@ -1,10 +1,11 @@
 # Here, we need to load the runtime symbols for dynamic loading of kernels.
 import sys
 import os
+# We should first import torch before loading the runtime, because the runtime may depend on torch.
 import torch
 import torch.nn as nn
 
-# Initialize the Triton runtime symbols, compile a dummy kernel before importing KAS cpp bindings.
+# Initialize the Triton runtime symbols, by compiling a dummy kernel before importing KAS cpp bindings.
 class _initial_compile(nn.Module):
     def __init__(self):
         super().__init__()
@@ -22,8 +23,17 @@ _initial_compiled_kernel = torch.compile(_initial_kernel, backend='inductor', mo
 _initial_compiled_kernel(torch.ones([1, 3, 32, 32]).cuda())
 del _initial_kernel
 
+# Now import the bindings. First backup the flags.
+old_flags = sys.getdlopenflags()
+# But we need to resolve symbols for the main program first, because otherwise the symbols may be mistakenly replaced by the runtime.
+sys.setdlopenflags(os.RTLD_LOCAL | os.RTLD_NOW)
 import kas_cpp_bindings
+# Now we can safely load the runtime, adding the symbols to the global scope.
+sys.setdlopenflags(os.RTLD_GLOBAL | os.RTLD_NOW)
 import kas_runtime
+# Restore the original flags.
+sys.setdlopenflags(old_flags)
+del old_flags
 
 from .Assembler import Assembled, Assembler
 from .Explorer import Explorer
