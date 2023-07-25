8382ced747a98ae454e2a5f03e1f823cc98b7898
Chenggang Zhao <lyricz@yeah.net>
2023-07-25 09:11:03 +0800

    Change queue size

--- experiments/search/beam/algorithm.py
+++ experiments/search/beam/algorithm.py
@@ -6,7 +6,7 @@ from KAS.Node import Path
 
 
 class BeamAlgorithm:
-    max_queue_size = 1000
+    max_queue_size = 1000000
     max_final_iterations = 150
     expand_async_layers = 3
     noise_weight = 0.4
