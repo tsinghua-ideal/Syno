40ceb54b600f4b27c69030c545061a54cbb0ba16
timsu1104 <timsu20021104@gmail.com>
2023-10-16 20:27:47 +0800

    run_workers script.

---
+++ experiments/run_workers.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Usage: ./run_tmux.sh $(NUM_GPUS) $(ENV_NAME) $(ARGS)
+
+# Delete cache
+echo 'Deleting cache...'
+rm -rf .scheduler-cache
+
+# Check power.
+if ! (( $1 > 0 && ($1 & ($1 - 1)) == 0 )); then
+  echo 'Process number should be a power of 2'
+  exit
+fi
+
+# Log2 function.
+function log2 {
+  local x=0
+  for ((y = $1 - 1; y > 0; y >>= 1)); do
+    ((x = x + 1))
+  done
+  echo "$x"
+}
+
+# Create windows.
+tmux new -s "KAS" -d
+tmux selectp -t 0
+tmux splitw -h -p 50
+for ((i = 0; i < $(log2 "$1"); i ++)); do
+  for ((j = 0; j < 2 ** i; j ++)) do
+    tmux selectp -t $((j * 2 + 1))
+    tmux splitw -v -p 50
+  done
+done
+
+# Run.
+current_path=$(pwd)
+
+# Client.
+for ((i = 1; i <= $1; i ++)); do
+tmux send-keys -t "$i" "echo TMUX Pane $i" Enter
+tmux send-keys -t "$i" "mamba activate $2" Enter
+tmux send-keys -t "$i" "cd ${current_path}" Enter
+tmux send-keys -t "$i" "export CUDA_VISIBLE_DEVICES=$(($i - 1))" Enter
+tmux send-keys -t "$i" "sleep 5" Enter
+tmux send-keys -t "$i" "./launch_client.sh ${*:3} --kas-client-cache-dir /tmp/.client$(($i))-cache" Enter
+done
+
+# Attach.
+tmux attach -t "KAS"
