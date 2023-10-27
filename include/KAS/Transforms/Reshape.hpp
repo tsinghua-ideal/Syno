c5c4247d9bd625e3bab611a268cb3cd208c68cae
Yongqi-Zhuo <Yongqi-Zhuo@users.noreply.github.com>
2023-10-28 03:17:21 +0800

    Add Reshape.{hpp,cpp}

---
+++ include/KAS/Transforms/Reshape.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "KAS/Core/DimVisitor.hpp"
+
+
+namespace kas {
+
+struct ReshapeBlockNeighbors {
+    using Side = std::variant<std::monostate, const MergeOp *, const Reduce *>; // nullptr is not allowed.
+    Side left;
+    Side right;
+    using Self = ReshapeBlockNeighbors;
+    auto separatedBy(const MergeOp *separator) const -> std::pair<Self, Self>;
+    auto isAdjacentTo(const Self& rhs) const -> bool;
+    auto combinedWith(const Self& rhs) const -> Self;
+};
+
+// Canonicalize reshape.
+// First we only allow Split's above Merge's,
+// then we check for redundant Split's.
+// The rule is simple. After the sequence of Merge's, we obtain the smallest reshape blocks,
+// and if the blocks that are adjacent get combined by Split's again, this is illegal.
+struct ReshapeCanonicalizer: public BottomTopDimVisitor<ReshapeCanonicalizer, ReshapeBlockNeighbors> {
+    using Adjacent = ReshapeBlockNeighbors;
+    auto transform(const Iterator&) const -> Adjacent;
+    auto transform(const Reduce&) const -> Adjacent;
+    auto transform(const RepeatLikeOp::Input&) const -> Adjacent;
+    auto transform(const SplitLikeOp::Input& dim) const -> Adjacent;
+    auto transform(const MergeLikeOp::Input& dim) const -> std::pair<Adjacent, Adjacent>;
+};
+
+} // namespace kas
--- src/Transforms/Split.cpp
+++ src/Transforms/Split.cpp
@@ -1,67 +1,12 @@
 #include "KAS/Core/Graph.hpp"
 #include "KAS/Core/Reduce.hpp"
-#include "KAS/Transforms/Merge.hpp"
 #include "KAS/Transforms/PrimitiveOpStore.hpp"
+#include "KAS/Transforms/Reshape.hpp"
 #include "KAS/Transforms/Split.hpp"
 #include "KAS/Utils/Common.hpp"
 
 
 namespace kas {
-
-auto ReshapeBlockNeighbors::separatedBy(const MergeOp *separator) const -> std::pair<Self, Self> {
-    KAS_ASSERT(separator);
-    return {
-        { left, separator },
-        { separator, right },
-    };
-};
-
-auto ReshapeBlockNeighbors::isAdjacentTo(const Self& rhs) const -> bool {
-    return (
-        // They originate from the very same MergeOp.
-        std::holds_alternative<const MergeOp *>(right) && std::holds_alternative<const MergeOp *>(rhs.left)
-        && std::get<const MergeOp *>(right) == std::get<const MergeOp *>(rhs.left)
-    ) || (
-        // Or they are commutative reductions. Actually we should check whether the reductions are commutative.
-        // But we only have sum reduction for the time being.
-        std::holds_alternative<const Reduce *>(right) && std::holds_alternative<const Reduce *>(rhs.left)
-    );
-}
-
-auto ReshapeBlockNeighbors::combinedWith(const Self& rhs) const -> Self {
-    KAS_ASSERT(!isAdjacentTo(rhs));
-    return { left, rhs.right };
-}
-
-auto ReshapeCanonicalizer::transform(const Iterator&) const -> Adjacent {
-    return {};
-}
-
-auto ReshapeCanonicalizer::transform(const Reduce& reduction) const -> Adjacent {
-    return { &reduction, &reduction };
-}
-
-auto ReshapeCanonicalizer::transform(const RepeatLikeOp::Input&) const -> Adjacent {
-    return {};
-}
-
-auto ReshapeCanonicalizer::transform(const SplitLikeOp::Input& dim) const -> Adjacent {
-    if (auto split = dynamic_cast<const SplitOp::Input *>(&dim); split) {
-        auto op = split->getOp();
-        return at(op->outputLhs).combinedWith(at(op->outputRhs));
-    } else {
-        return {};
-    }
-}
-
-auto ReshapeCanonicalizer::transform(const MergeLikeOp::Input& dim) const -> std::pair<Adjacent, Adjacent> {
-    if (auto merge = dynamic_cast<const MergeOp::Input *>(&dim); merge) {
-        auto op = merge->getDerivedOp<MergeOp>();
-        return at(op->output).separatedBy(op);
-    } else {
-        return {};
-    }
-}
 
 SplitOp::SplitOp(const Dimension& outputLhs, const Dimension& outputRhs):
     SplitLikeOp { outputLhs, outputRhs },
