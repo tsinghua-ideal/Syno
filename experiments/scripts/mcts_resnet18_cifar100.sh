6441306351d7a8e444ce1797b6f669d49cf41746
timsu1104 <timsu20021104@gmail.com>
2023-11-12 20:22:11 +0800

    Update results.md and script.

--- experiments/scripts/mcts_resnet18_cifar100.sh
+++ experiments/scripts/mcts_resnet18_cifar100.sh
@@ -1,13 +1,8 @@
-./run_tmux.sh 8 kas \
---kas-server-save-dir results/cifar100-session-v20231021 --kas-server-addr 0.0.0.0 --kas-server-port 7070 \
---kas-search-algo MCTS \
---model torchvision/resnet18 --sched cosine --epoch 50 --batch-size 512 --warmup-epochs 0 --cooldown-epochs 0 \
---lr 0.1 --momentum 0.9 --weight-decay 0.001 \
---root ~/data --dataset cifar100 --num-classes 100 \
---compile \
---kas-sampler-workers 64 --kas-num-virtual-evaluator 8 --kas-reward-power 4 \
---kas-acc-lower-bound 0.01 --kas-acc-upper-bound 0.7 --kas-server-save-interval 1800 \
---kas-inference-time-limit 100 \
---kas-max-flops-ratio 0.35 --kas-max-enumerations 5 --kas-max-finalizations 2 \
---kas-depth 16 --kas-max-dim 13 --kas-max-reductions 5 --kas-max-merges 2 --kas-max-splits 2 --kas-max-shifts 3 --kas-max-strides 2 \
---kas-max-size-multiplier 16 --kas-max-variables-in-size 3 --kas-max-chain-length 5 --kas-max-shift-rhs 5
+bash ./run_server.sh 8 torch KAS \
+--kas-server-addr 0.0.0.0 --kas-server-port 7070 --kas-server-save-dir results/cifar100-session-v20231112-2 --kas-server-save-interval 3600\
+ --kas-search-algo MCTS \
+ --model torchvision/resnet18 \
+ --kas-sampler-workers 400 --kas-num-virtual-evaluator 16 \
+ --kas-reward-power 4 --kas-acc-lower-bound 0.01 --kas-acc-upper-bound 0.75 \
+ --kas-inference-time-limit 45 \
+ --kas-max-flops-ratio 0.9 --kas-max-enumerations 5 --kas-max-finalizations 2 --kas-depth 12 --kas-max-reductions 5 --kas-max-merges 2 --kas-max-splits 2 --kas-max-shifts 2 --kas-max-strides 2 --kas-max-size-multiplier 16 --kas-max-variables-in-size 3 --kas-max-chain-length 7 --kas-max-shift-rhs 5 --kas-max-expansion-merge-multiplier 2048 --kas-max-weight-share-dim 16
