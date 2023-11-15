a8742f421aeb0fa81d567b07c3222884d2e4e1cd
timsu1104 <timsu20021104@gmail.com>
2023-11-15 18:39:08 +0800

    Update mcts script.

--- experiments/scripts/mcts_resnet18_cifar100.sh
+++ experiments/scripts/mcts_resnet18_cifar100.sh
@@ -1,9 +1,8 @@
 bash ./run_server.sh 8 torch KAS \
---kas-server-addr 0.0.0.0 --kas-server-port 7070 --kas-server-save-dir results/cifar100-session-v20231112-2 --kas-server-save-interval 3600\
+--kas-server-addr 0.0.0.0 --kas-server-port 7070 --kas-server-save-dir results/cifar100-session-v20231114-NCHH --kas-server-save-interval 3600\
  --kas-search-algo MCTS \
  --model torchvision/resnet18 \
- --kas-sampler-workers 400 --kas-num-virtual-evaluator 16 \
+ --kas-sampler-workers 400 --kas-num-virtual-evaluator 4 \
  --kas-reward-power 4 --kas-acc-lower-bound 0.01 --kas-acc-upper-bound 0.75 \
- --kas-inference-time-limit 45 \
  --kas-max-flops-ratio 0.9 --kas-min-flops-ratio 0.15 \
  --kas-max-enumerations 5 --kas-max-finalizations 2 --kas-depth 12 --kas-max-reductions 5 --kas-max-merges 4 --kas-max-splits 4 --kas-max-shifts 2 --kas-max-strides 0 --kas-max-size-multiplier 16 --kas-max-variables-in-size 3 --kas-max-chain-length 7 --kas-max-shift-rhs 2 --kas-max-expansion-merge-multiplier 2048 --kas-max-weight-share-dim 16 --client-mem-limit 0.8
