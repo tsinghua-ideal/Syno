6796989b964d31d55f992a1b66588aa35b946cd8
timsu1104 <timsu20021104@gmail.com>
2023-11-19 23:17:30 +0800

    Update mcts script.

--- experiments/scripts/mcts_resnet18_cifar100.sh
+++ experiments/scripts/mcts_resnet18_cifar100.sh
@@ -5,4 +5,4 @@ bash ./run_server.sh 8 torch KAS \
  --kas-sampler-workers 400 --kas-num-virtual-evaluator 4 \
  --kas-reward-power 4 --kas-acc-lower-bound 0.4 --kas-acc-upper-bound 0.7 \
  --kas-max-flops-ratio 0.9 --kas-min-flops-ratio 0.15 \
- --kas-max-enumerations 5 --kas-max-finalizations 2 --kas-depth 12 --kas-max-reductions 5 --kas-max-merges 1 --kas-max-splits 3 --kas-max-shifts 2 --kas-max-strides 0 --kas-max-size-multiplier 4 --kas-max-variables-in-size 3 --kas-max-chain-length 6 --kas-max-shift-rhs 2 --kas-max-expansion-merge-multiplier 2048 --kas-min-weight-share-dim 8 --kas-max-weight-share-dim 8 --kas-min-unfold-ratio 2.3 --client-mem-limit 0.8
+ --kas-max-enumerations 5 --kas-max-finalizations 2 --kas-depth 12 --kas-max-reductions 5 --kas-max-merges 1 --kas-max-splits 3 --kas-max-shifts 2 --kas-max-strides 0 --kas-max-size-multiplier 4 --kas-max-variables-in-size 3 --kas-max-chain-length 6 --kas-max-shift-rhs 2 --kas-max-expansion-merge-multiplier 2048 --kas-min-weight-share-dim 8 --kas-max-weight-share-dim 8 --kas-min-unfold-ratio 2.3 --client-mem-limit 1.0
