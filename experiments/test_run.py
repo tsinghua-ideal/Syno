d7f71a4eea14ea8443225411695ea3164323a2aa
Yongqi-Zhuo <Yongqi-Zhuo@users.noreply.github.com>
2023-11-28 10:52:54 +0800

    Do not codegen again.

--- experiments/test_run.py
+++ experiments/test_run.py
@@ -8,7 +8,7 @@ import ctypes
 import shutil
 import torch
 import thop
-from KAS import Path, init_weights
+from KAS import Path, init_weights, KernelLoader
 
 from base import log, models, parser, dataset, trainer
 
@@ -22,7 +22,6 @@ if __name__ == "__main__":
     logging.info("Loading dataset ...")
     train_dataloader, val_dataloader = dataset.get_dataloader(args)
 
-    
     logging.info("Preparing model ...")
     if args.kas_use_orig_model:
         if args.model.startswith("torchvision/"):
@@ -43,34 +42,14 @@ if __name__ == "__main__":
             model = torch.compile(model)
     else:
         try:
-            model, sampler = models.get_model(args, return_sampler=True)
-            node = sampler.visit(Path.deserialize(path))
-            if node is None:
-                logging.error(f"{Path.deserialize(path)} is not in the search space. ")
-                for subpath in Path.deserialize(path).hierarchy:
-                    if sampler.visit(subpath) is None:
-                        logging.warning(f"Subpath {subpath} is not valid")
-                        logging.info(f"Available Children of {node._node}:")
-                        for child in node.get_children_handles():
-                            child_node = node.get_child(child)
-                            if child_node is None:
-                                continue
-                            logging.info(
-                                f"\t{child}:\t{node.get_child_description(child)}"
-                            )
-                        break
-                    else:
-                        node = sampler.visit(subpath)
-                exit(1)
-            node = node.to_node()
-            if path:
-                kernel_loader = sampler.realize(model, node)
-                model.load_kernel(
-                    kernel_loader,
-                    compile=args.compile,
-                    batch_size=args.batch_size,
-                    seq_len=args.gpt_seq_len,
-                )
+            kernel_loader = KernelLoader.from_directory(os.path.join(path, "kernel_scheduler_dir"))
+            model = models.get_model(args, return_sampler=False)
+            model.load_kernel(
+                kernel_loader,
+                compile=args.compile,
+                batch_size=args.batch_size,
+                seq_len=args.gpt_seq_len,
+            )
         except Exception as e:
             if not "out of memory" in str(e):
                 raise e
@@ -113,49 +92,16 @@ if __name__ == "__main__":
     nparams = params
     kernel_flag = "LOAD_SUCCESS"
 
-    save_dir = args.kas_server_save_dir
-
-    node = sampler.visit(Path.deserialize(path)).to_node()
-
-    os.makedirs(save_dir, exist_ok=True)
-
-    acc_str = (
-        ("0" * max(0, 5 - len(f"{int(accuracy * 10000)}"))) + f"{int(accuracy * 10000)}"
-        if accuracy >= 0
-        else "ERROR"
-    )
-    hash_str = f"{ctypes.c_size_t(hash(path)).value}"
-    kernel_save_dir = os.path.join(save_dir, "_".join([acc_str, hash_str]))
-    if not (os.path.exists(kernel_save_dir) and acc_str == "ERROR"):
-        os.makedirs(kernel_save_dir, exist_ok=True)
-
-        # GraphViz
-        node.generate_graphviz_as_final(
-            os.path.join(kernel_save_dir, "graph.dot"), "kernel"
+    # Meta information
+    with open(os.path.join(path, "test_run_meta.json"), "w") as f:
+        json.dump(
+            {
+                "accuracy": accuracy,
+                "flops": flops,
+                "params": nparams,
+                "kernel_flag": kernel_flag,
+                "loss": 0,
+            },
+            f,
+            indent=2,
         )
-
-        # Loop
-        with open(os.path.join(kernel_save_dir, "loop.txt"), "w") as f:
-            f.write(str(node.get_nested_loops_as_final()))
-
-        # Meta information
-        with open(os.path.join(kernel_save_dir, "meta.json"), "w") as f:
-            json.dump(
-                {
-                    "path": path,
-                    "accuracy": accuracy,
-                    "flops": flops,
-                    "params": nparams,
-                    "kernel_flag": kernel_flag,
-                    "loss": 0,
-                },
-                f,
-                indent=2,
-            )
-
-        # copying kernel dir
-        if kernel_flag != "MOCKPATH":
-            shutil.copytree(
-                sampler.realize(model, node).get_directory(),
-                os.path.join(kernel_save_dir, "kernel_scheduler_dir"),
-            )
