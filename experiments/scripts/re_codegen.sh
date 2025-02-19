6ae276c3dbd887ce8ad1a8f1a945d6abf594a6f2
Yongqi-Zhuo <Yongqi-Zhuo@users.noreply.github.com>
2025-02-19 18:46:09 +0800

    Update re_codegen

--- experiments/scripts/re_codegen.sh
+++ experiments/scripts/re_codegen.sh
@@ -6,8 +6,17 @@ ROOTDIR=${1:-./results}
 
 echo "Using root directory: $ROOTDIR"
 
-echo "Regenerating kernel scheduler directories for resnet good kernels......"
-python analysis/re_codegen.py --model torchvision/resnet18 --batch-size 1 --kas-sampler-workers 400 \
+rm -rf .recodegen-scheduler-cache
+
+# # ResNet-18 cannot share codegen with ResNet-34, need to rerun codegen.
+# echo "Regenerating kernel scheduler directories for resnet18 good kernels......"
+# python analysis/re_codegen.py --model torchvision/resnet18 --batch-size 1 --kas-sampler-workers 400 \
+#  --kas-max-flops-ratio 0.9 --kas-min-flops-ratio 0.15 \
+#  --kas-max-enumerations 5 --kas-max-finalizations 2 --kas-depth 12 --kas-max-reductions 5 --kas-max-merges 1 --kas-max-splits 3 --kas-max-shifts 2 --kas-max-strides 0 --kas-max-size-multiplier 4 --kas-max-variables-in-size 3 --kas-max-chain-length 6 --kas-max-shift-rhs 2 --kas-max-expansion-merge-multiplier 2048 --kas-min-weight-share-dim 8 --kas-max-weight-share-dim 8 --kas-min-unfold-ratio 2.3 --client-mem-limit 1.0 --kas-scheduler-cache-dir .recodegen-scheduler-cache \
+#  --dirs $(find $ROOTDIR/resnet-good-kernels -mindepth 1 -maxdepth 1 -type d -regex ".*/0\.[0-9]x")
+
+echo "Regenerating kernel scheduler directories for resnet34 good kernels......"
+python analysis/re_codegen.py --model torchvision/resnet34 --batch-size 1 --kas-sampler-workers 400 \
  --kas-max-flops-ratio 0.9 --kas-min-flops-ratio 0.15 \
  --kas-max-enumerations 5 --kas-max-finalizations 2 --kas-depth 12 --kas-max-reductions 5 --kas-max-merges 1 --kas-max-splits 3 --kas-max-shifts 2 --kas-max-strides 0 --kas-max-size-multiplier 4 --kas-max-variables-in-size 3 --kas-max-chain-length 6 --kas-max-shift-rhs 2 --kas-max-expansion-merge-multiplier 2048 --kas-min-weight-share-dim 8 --kas-max-weight-share-dim 8 --kas-min-unfold-ratio 2.3 --client-mem-limit 1.0 --kas-scheduler-cache-dir .recodegen-scheduler-cache \
  --dirs $(find $ROOTDIR/resnet-good-kernels -mindepth 1 -maxdepth 1 -type d -regex ".*/0\.[0-9]x")
